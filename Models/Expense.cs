96b3d164642f0c940ed80f42ec89eba61976c077
Lau <laurentiucozma12@gmail.com>
2023-10-27 16:28:39 +0300

    Add project files.

---
+++ Models/Expense.cs
@@ -0,0 +1,10 @@
+﻿namespace Cozma_Laurentiu_Lab6.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Cozma_Laurentiu_Lab6.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options) : base(options)
+        {
+        
+        }
+
+        public DbSet<Expense> Expense { get; set; }
+    }
+}
